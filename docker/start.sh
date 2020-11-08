11dec7b3220e6ab0d013ffd9aa5152825c19c3fc
demo2099 <available3099@gmail.com>
2020-11-08 15:34:22 +0800

    init

--- docker/start.sh
+++ docker/start.sh
@@ -11,3 +11,10 @@ docker build -t jscooldocker .
 # docker run   -e TZ="Asia/Shanghai"   -d --net=host --name=jscooldocker jscooldocker
 
 #echo "$var启动完成"
+
+docker-compose up -d #启动；
+#docker-compose logs #打印日志；
+#docker-compose pull #更新镜像；
+#docker-compose stop #停止容器；
+#docker-compose restart #重启容器；
+#docker-compose down #停止并删除容器；
